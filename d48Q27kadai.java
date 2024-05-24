e85983d38c93f21460da928cc2e16a7e28283249
itc-s23024 <s23024@std.it-college.ac.jp>
2024-05-24 11:58:47 +0900

    5/23 第４８回 問４(27)

---
+++ d48Q27kadai.java
@@ -0,0 +1,11 @@
+//s23024
+//d48Q27のエラーコードを治す 5行目
+
+public class d48Q27kadai {
+    public static void main (String[] args) {
+        int i = 0;
+        while (i <= 6) {
+            System.out.print(i);
+        }
+    }
+}
