a10db405d96d5510cc3d9116998c35eb52eef5d1
itc-s2304 <s23024@std.it-college.ac.jp>
2024-08-16 12:50:59 +0000

    8/16 課題完成

---
+++ 6.28kadai/d49Q6kadai01.java
@@ -0,0 +1,20 @@
+// d49Q6kadai01 s23024
+/* テキストp35 StringBuilderクラスを使って与えられた引数を連結し、表示する課題
+実行の際は引数を受け付けるよう設計
+【実行例】
+> java d49Q6kadai01 IT カレッジ 沖縄
+ITカレッジ沖縄
+*/
+
+import java.lang.StringBuilder;
+
+public class d49Q6kadai01 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0;  i < args.length; i++) {
+			sb.append(args[i]);
+		}
+		System.out.println(sb.toString());
+	}
+}
+		
